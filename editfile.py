4ff7c6fb36825eb1a2015d1545220c487aca8f06
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-10 14:41:41 -0800

    A basic model of joining /splitting a file is drawn which would be later updated with respect to code requirements

---
+++ editfile.py
@@ -0,0 +1,74 @@
+import os
+
+
+
+# A basic model of splitting a file is drawn which would be later updated as per reqirement
+#split a file into a set of portions which will be used by p2p application to determine which parts to be sent.
+#These file once recived will be joint back to the original format
+def splitfile():#filepos, destpos,chunksize):
+    filepos = raw_input('enter the postion of file to be split: ')
+    destpos = raw_input('enter the position of destination folder: ')
+    data_size = os.path.getsize(filepos)  # geet size of data at file to be split
+    chunksize = data_size / 4  # split file into 5 pieces
+
+    try:
+        partnum = 0
+        input = open(filepos, 'rb')  # use binary read mode to open the file
+        while 1:
+            chunk = input.read(chunksize)  # read only the chunksized amount of data in chunk variable
+            if not chunk: break
+            partnum = partnum + 1  # increment partnum by 1 everytime a chunk is updated
+            filename = os.path.join(destpos, (
+            'part%0d' % partnum))  # create a binary file called filename at destination directory
+            fileobj = open(filename, 'wb')  # open the file 'filename' in binary write mode
+            fileobj.write(chunk)  # writh chunk data in the file
+            fileobj.close()
+        input.close()
+
+        print ' split finished in: ', partnum, ' parts'
+        return
+
+    except:
+        print' error splitting'
+        return
+
+    return
+
+
+
+#a basic model of joining a multiple splitted file back to the original format
+def joinfile():#fromdir, destfile):
+    fromdir = raw_input('enter the directory from where file has to be joined: ')
+    filename = 'filename'
+    destfile = '{0}.zip'.format(filename)
+    try:
+        readsize = 1023  # set readsize to 1byte
+        outputfile = open(destfile, 'wb')  # open a file with desired name in binary write mocde
+        parts = os.listdir(fromdir)  # get the list of all files from the directory to be joint
+        parts.sort()  # and sort them in correect order
+        for filename in parts:
+            filepath = os.path.join(fromdir, filename)  # create a directory
+            fileobj = open(filepath, 'rb')  # open the file in that directory in biinary read mode
+            while 1:
+                filesizebytes = fileobj.read(
+                    readsize)  # read data from  the parts at 1023 bits per read. This is done to prevent memory congestion
+                if not filesizebytes: break
+                outputfile.write(filesizebytes)  # write the data in the named file
+            fileobj.close()
+        outputfile.close()
+    except:
+        print'join failed'
+    else:
+        print 'join complete'
+
+
+    #deleting the split files created during the file sharing after joining them.
+    fileList = os.listdir(fromdir)
+    for fileName in fileList:
+        os.remove(fromdir + "/" + fileName)
+    print' directory deleted'
+
+    return
+
+
+joinfile(
