6dd00d313b4f2b515f5bfc237fd71061a71ca736
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-11 13:47:43 -0800

    Search file in a directory and return the file pos

---
+++ searchfile.py
@@ -0,0 +1,16 @@
+# search directory used by seeder to search for the file on the disk in a particular directory
+
+import os
+
+def findfile(name,path):
+    for root,dirs ,files in os.walk(path):
+        if name in files or name in dirs:
+
+            filepos= os.path.join(root,name)
+            print'file found'
+            return filepos
+        else: return 'file not found'
+
+
+#returns the postion of the file in the directory
+
