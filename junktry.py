6fdcbb9f7b5b99a111d8a263d64bcda5fa98326c
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-11 18:39:12 -0800

    to be deleted

---
+++ junktry.py
@@ -0,0 +1,10 @@
+from searchfile import *
+import editfile
+def main():
+    path= '/home/deepak/PycharmProjects/bittorrent_service'
+    #n=raw_input('enter the file to be searched: ')
+    #name=n+'.zip'
+    #filepos=findfile(name,path)
+    editfile.joinfile()
+
+main()
