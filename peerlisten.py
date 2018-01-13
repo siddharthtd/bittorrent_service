efbe071fbdf4a4338bd411ef38e16bd56c54c501
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-12 17:18:21 -0800

    Socket designed that listens to connections from other peers
    *To be modified in future

--- peerlisten.py
+++ peerlisten.py
@@ -1,12 +1,14 @@
+# This code is designed to make a socket to listen  for peers trying to connect
 import socket
 
-def listensocket(): #port,
+def listensocket(): #port
     port= 10
-    backlog=5
-    connection=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-    connection.bind(('',port))
-    connection.listen(backlog)
+    backlog=5 #Number of requests to be queued
+    connection=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #creating a socket that communicates with IPv4 protocol and TCP
+    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1) #setting socket option: value 1 to make the port reuasabe after every 1 exit.
+                                                                    #this prevents the port already use error
+    connection.bind(('',port))  #bind port to receive connections
+    connection.listen(backlog) #start listening for ques ranging till backlog=5
     print 'host listening on '+str(connection)
     return connection
 listensocket()
