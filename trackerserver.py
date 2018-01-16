d8b03b0e56fae9b35701b134476172f5f9d1a796
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-16 14:26:29 -0800

    Server receives file from peer and stores it at desired location. to be modified for storing in database

--- trackerserver.py
+++ trackerserver.py
@@ -1,9 +1,11 @@
-# This code is designed to make a socket to listen  for peers trying to connect
+# This code is designed to make a socket to listen  for peers trying to connect.
+#the server connects to the peer, receives file from peer and save it at the given location
 import socket
+import os
 
 def listensocket(): #port
-    host='' #generic name
-    port= 8888
+    host=socket.gethostname() #gethostname
+    port= 8882
     backlog=10 #Number of requests to be queued
     connection=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #creating a socket that communicates with IPv4 protocol and TCP
     connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1) #setting socket option: value 1 to make the port reuasabe after every 1 exit.
@@ -19,9 +21,24 @@ def listensocket(): #port
     while 1:
         conn,addr= connection.accept()
         print 'connected with '+ addr[0]+':'+str(addr[1])
+        receivefile(conn)
     connection.close()
 
 
 
-    return connection
+def receivefile(sockdata):
+    filename= 'filename'+'.zip'
+    path= 'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles\serverdb'
+    filepath = os.path.join(path, filename)
+    outputfile=open(filename,'wb')
+
+    while True:
+        data=sockdata.recv(1023) #buffer size receive
+        print 'data received'
+        if not data: break
+        outputfile.write(data)
+    outputfile.close()
+    print'done receiving'
+    return
+
 listensocket()
