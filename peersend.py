5f84414d05b4d48d10da702c40f6b0b5bf1e194e
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-16 14:27:56 -0800

    Correction: Peer sending data to server

---
+++ peersend.py
@@ -0,0 +1,29 @@
+#Sending a file to server at given local host port
+import socket
+import os
+
+
+def listenport():
+    s=socket.socket()
+    host=socket.gethostname()
+    port=8882
+
+    s.connect((host,port)) #connect to the host
+    filename='Documents'+'.zip'
+    path=r'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles'
+    filepath=os.path.join(path,filename)
+    f= open(filepath,'rb')
+    l=f.read(1023)
+    #till reading, read and send l sized data
+    while (l):
+        try:
+            s.send(l)
+            print ('Sent',repr(l))
+            l=f.read(1024)
+        except KeyboardInterrupt:
+            print' interruppted'
+    f.close()
+    print 'done sending'
+listenport()
+
+
