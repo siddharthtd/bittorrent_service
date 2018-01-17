3ea0581e1d482ba51c03a8b00b228614d172fbe4
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-17 15:42:44 -0800

    Temp db created .. to be deleted afterwards

---
+++ listfilesPeer.py
@@ -0,0 +1,33 @@
+
+
+#getting list of all the files in the directory and returning filename, path and size of the file in the disctionary format
+#input : Path of file on the system. doesn't work with finding directory
+#return: disctionary 'filedetails'
+
+import os
+
+def filedetails(path):
+    dir = os.listdir(path) #get list of all files from path
+    fileprop={'name':'','path':'','size':''}
+
+    for files in dir:
+        filepos= os.path.join(path,files)
+        filesize=os.path.getsize(filepos) #get file size
+        filesize= filesize/1024 #convert into KB
+
+        fileprop.update({'name':files,'path':filepos,'size':filesize})
+        print fileprop
+        #testdb.addvalues(fileprop)
+        print 'done'
+
+
+    return
+
+
+
+
+def main():
+    path = r'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles'
+    filedetails(path)
+
+main()
---
+++ testdb.py
@@ -0,0 +1,19 @@
+#a temporary databse is created to check the functioning of db
+"""TO BE DELETED AFTERWARDS"""
+import sqlite3
+
+con=sqlite3.connect(r'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles\serverdb\server.db')
+cursor= con.cursor()
+
+def createtable():
+    cursor.execute("CREATE TABLE IF NOT EXISTS filedetails(filename PRIMARY KEY NOT NULL, filepos TEXT NOT NULL, filesize integer )")
+
+def addvalues(fileprop):
+    cursor.execute("INSERT INTO  filedetails(filename,filepos,filesize) VALUES (?, ?, ?)",[fileprop['name'],fileprop['path'],fileprop['size']])
+    print 'value entry successful'
+    con.commit()
+def checkvalues():
+
+    for row in cursor.fetchall():
+
+
