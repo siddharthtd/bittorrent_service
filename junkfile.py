e593433331fea4169897025484998156c186efcf
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-22 19:30:46 -0800

    testdb and junkfile updated

--- junkfile.py
+++ junkfile.py
@@ -1,33 +1,56 @@
-
-#getting list of all the files in the directory and returning filename, path and size of the file in the disctionary format
-#input : Path of file on the system. doesn't work with finding directory
-#return: disctionary 'filedetails'
-
 import os
 import testdb
+import peer_id_gen
+
 
 def filedetails(path):
-    dir = os.listdir(path) #get list of all files from path
-    fileprop={'name':'','path':'','size':''}
+    dir= os.listdir(path)
+    fileprop= {'name':'','peerid': '','size':''}
+    result =testdb.checkvalues(fileprop)
+    if (result== False):
+        print 'generating new file details section'
+        peerid= peer_id_gen.peer_id()
+        for files in dir:
+            filepos = os.path.join(path, files)
+            filesize = os.path.getsize(filepos)  # get file size
+            filesize = filesize / 1024  # convert into KB
 
-    for files in dir:
-        filepos= os.path.join(path,files)
-        filesize=os.path.getsize(filepos) #get file size
-        filesize= filesize/1024 #convert into KB
-
-        fileprop.update({'name':files,'path':filepos,'size':filesize})
-        print fileprop
-        testdb.addvalues(fileprop)
-        print 'done'
-
-
+            fileprop.update({'name': files, 'peerid': peerid, 'size': filesize})
+            checkresult(fileprop,files)
     return
 
 
+def checkresult(fileprop,files):
+            result = testdb.checkvalues(fileprop)
+            while (result== False):
+                print'new peer id accepted'
+                testdb.addvalues(fileprop)
+                return
+
+            while (result== True):
+                print ' new id to be created'
+                peerid=peer_id_gen.peer_id()
+                filesize=fileprop['size']
+                fileprop.update({'name': files, 'peerid': peerid, 'size': filesize})
+                testdb.checkvalues(fileprop)
+                checkresult(fileprop,files)
+                return
 
 
-def main():
+
+
+def  main():
     path = r'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles'
     filedetails(path)
 
+
 main()
+
+
+
+
+
+
+
+
+
