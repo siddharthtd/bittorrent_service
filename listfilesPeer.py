c3229cf78496eb5fb1238f1c4a274b7264f20ec5
deepakdevaru <joshi.deepak26@gmail.com>
2018-01-24 18:13:00 -0800

    testdb and listfilesPeer updated

--- listfilesPeer.py
+++ listfilesPeer.py
@@ -1,37 +1,63 @@
-# getting list of all the files in the directory and returning filename, path and size of the file in the dictionary format
-# input : Path of file on the system. doesn't work with finding directory
-# return: disctionary 'filedetails'
-
+"""Input 1: file directory path from where list of all files present are to be gathered"""
+"""Input Link 1: Unique peer id from peer_id_gen to develop unique peer id """
+"""Output Link 1: Output send to testdb.py to add data( peerid, name and size) to table , check for peerid duplicates.
+    Output being fileprop(peerid,filename,filesize)"""
+"""Main() module to be edited"""
 import os
 import testdb
 import peer_id_gen
 
-
+#filedetails method takes input directory path.crates a dictionary containing peerid,
+# filename and filesize of each files in the directory.
+#The dictinary is fowared to checkresults to chec whether the duplicate entry of perrid.
+#The random peerid is generated in peer_id_gen class
 def filedetails(path):
-    dir = os.listdir(path)  # get list of all files from path
-    fileprop = {'name':'', 'peerid':'', 'size':''}
-    result = testdb.checkvalues(fileprop)
-    peerid = peer_id_gen.peer_id()
-    while (result == True):
-        print "control stuck"
-        result = testdb.checkvalues(fileprop)
-    for files in dir:
-        filepos = os.path.join(path, files)
-        filesize = os.path.getsize(filepos)  # get file size
-        filesize = filesize / 1024  # convert into KB
+    dir= os.listdir(path)
+    fileprop= {'name':'','peerid': '','size':''}
+    result =testdb.checkvalues(fileprop)
+    if (result== False):
+        print 'generating new file details section'
+        peerid= peer_id_gen.peer_id()
+        print ' peer id gen method getting executed'
+        for files in dir:
+            filepos = os.path.join(path, files)
+            filesize = os.path.getsize(filepos)  # get file size
+            filesize = filesize / 1024  # convert into KB
 
-        fileprop.update({'name': files, 'peerid': peerid, 'size': filesize})
-
-        # result=testdb.checkvalues(fileprop)
-
-    print 'done'
-
+            fileprop.update({'name': files, 'peerid': peerid, 'size': filesize})
+            checkresult(fileprop,files)
     return
 
+#this module checks for duplecate entry of peer id by comparing
+#it with the peerid entries in the db . If duplicate entry found, generate a new peerid
+#else send peerid , filename and filesize to database.
+#testdb.checkvalues returns true if dupicate entry present in database. The code retrives true or false
+# by calling testdb.checkvalues() module and stores data in db by calling testdb.checkvalues() module
+def checkresult(fileprop,files):
 
-def main():
+            print ' check results executing'
+            result = testdb.checkvalues(fileprop)
+            while (result== False):
+                print'new peer id accepted'
+                testdb.addvalues(fileprop)
+                return
+
+            while (result== True):
+                print ' new id to be created'
+                peerid=peer_id_gen.peer_id()
+                filesize=fileprop['size']
+                fileprop.update({'name': files, 'peerid': peerid, 'size': filesize})
+                testdb.checkvalues(fileprop)
+                checkresult(fileprop,files)
+                return
+
+
+
+
+def  main():
     path = r'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles'
     filedetails(path)
-
-
 main()
+
+
+
--- testdb.py
+++ testdb.py
@@ -1,36 +1,29 @@
 #a temporary databse is created to check the functioning of db
-"""TO BE DELETED AFTERWARDS"""
+#input: fileprop from listfilersPeer.py
+#output: table
+#DB store location to be altered according to needs
 import sqlite3
-import json
-import peer_id_gen
 
-
-con=sqlite3.connect(r'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles\serverdb\try29.db')
+con=sqlite3.connect(r'C:\Users\Deepak\PycharmProjects\bittorrent_service\Testfiles\serverdb\try41.db')
 con.row_factory= lambda cursor,row:row[0]
 cursor= con.cursor()
 def createtable():
-    cursor.execute("CREATE TABLE IF NOT EXISTS filedetails(peerid TEXT , filename TEXT , filesize integer )")
+    cursor.execute("CREATE TABLE IF NOT EXISTS filedetails(peerid TEXT, filename TEXT NOT NULL UNIQUE , filesize integer )")
 
 def addvalues(fileprop):
-
-        cursor.execute("INSERT INTO filedetails(peerid,filename,filesize) VALUES (?, ?, ?)",[fileprop['peerid'],fileprop['name'],fileprop['size']])
+        cursor.execute("INSERT OR IGNORE INTO filedetails(peerid,filename,filesize) VALUES (?, ?, ?)",[fileprop['peerid'],fileprop['name'],fileprop['size']])
         print 'value entry successful'
         print ''
         print ''
         con.commit()
-# def ignore(fileprop):
-#     cursor.execute("INSERT OR IGNORE INTO filedetails(peerid,filename,filesize) VALUES (?, ?, ?)",[fileprop['peerid'],fileprop['name'],fileprop['size']])
 
 def checkvalues(fileprop):
-    con.row_factory= lambda cursor,row:row[0]
     data=cursor.execute('SELECT peerid FROM filedetails').fetchall()
+
     if fileprop['peerid'] in data:
-        print' peer id already present'
+        print' peerid  present'
         return True
-
     else:
-        print ' peer id not present'
         return False
-
 
 createtable()
